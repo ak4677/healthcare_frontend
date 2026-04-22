81fa35a09009d11a990690f24edeb445881d56ca
aman kushwah <ak4677@srmist.edu.in>
2026-04-22 11:28:36 +0530

    minor error fix

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:'/healthcare_frontend',
+  base:'/healthcare_frontend/',
   plugins: [react(),tailwindcss()],
 })
